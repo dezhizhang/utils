22e2034bdd1baf8049b119101428c2115511c85e
dezhizhang <1541609448@qq.com>
2021-05-06 09:20:06 +0800

    update

---
+++ src/source.js
@@ -0,0 +1,41 @@
+
+/**
+ * 入口js
+ * 向外暴露工具函数
+*/
+
+//function方法
+export { apply } from './function/apply';
+export { call } from './function/call';
+export { bind } from './function/bind';
+
+//array方法
+export { unique1,unique2,unique3 } from './array/unique';
+export { concat } from './array/concat'; 
+export { slice } from './array/slice';
+export { map } from './array/map';
+export { reduce } from './array/reduce';
+export { filter } from './array/filter';
+export { find } from './array/find';
+export { findIndex } from './array/findIndex';
+export { every } from './array/every';
+export { some } from './array/some';
+export { compact } from './array/compact';
+export { chunk } from './array/chunk';
+export { difference } from './array/difference';
+export { pull } from './array/pull';
+export { drop,dropRight } from './array/drop';
+export { mergeArray,mergeArray1 } from './array/mergeArray';
+export { flatten1,flatten2 } from './array/flatten'
+
+//object方法
+export { newInstance } from './object/newInstance';
+export { mergeObject } from './object/mergeObject';
+export { clone1,clone2 } from './object/clone';
+export { deepClone } from './object/deepClone';
+
+//string方法
+export { reverseString } from './string/reverseString';
+export { truncate } from './string/truncate';
+export { palindrome } from './string/palindrome';
+export { maxRepeatChar } from './string/maxRepeatChar';
